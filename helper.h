00fd80f34a843edf37d89a6ab105187b088784c8
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-30 15:23:30 -0500

    helper.h: modify the cap function to change all characters to uppercase

--- helper.h
+++ helper.h
@@ -30,84 +30,20 @@
 
 /*  Function declarations  */
 
-ssize_t Readline(int fd, void *vptr, size_t maxlen);
-ssize_t Writeline(int fc, const void *vptr, size_t maxlen);
 void Cap(char []);
 char* ReadFile(char *);
 
 void Cap(char string[]){
     int i;
     int x = strlen(string); // get the length of the whole string.
-    if (isalpha(string[0]))
-    {
-        string[0] = toupper(string[0]);
-    }
-    for (i=1;i<x;i++){
-        if (isalpha(string[i]) && string[i-1] == ' '){
-            // only first letters of a word.
+    for (i=0;i<x;i++){
+        if (isalpha(string[i])){
             string[i]= toupper(string[i]);
         }
     }
 }
 
 
-
-/*  Read a line from a socket  */
-
-ssize_t Readline(int sockd, void *vptr, size_t maxlen) {
-    ssize_t n, rc;
-    char    c, *buffer;
-
-    buffer = vptr;
-
-    for ( n = 1; n < maxlen; n++ ) {
-
-        if ( (rc = read(sockd, &c, 1)) == 1 ) {
-            *buffer++ = c;
-            if ( c == '\n' )
-                break;
-        }
-        else if ( rc == 0 ) {
-            if ( n == 1 )
-                return 0;
-            else
-                break;
-        }
-        else {
-            if ( errno == EINTR )
-                continue;
-            return -1;
-        }
-    }
-
-    *buffer = 0;
-    return n;
-}
-
-
-/*  Write a line to a socket  */
-
-ssize_t Writeline(int sockd, const void *vptr, size_t n) {
-    size_t      nleft;
-    ssize_t     nwritten;
-    const char *buffer;
-
-    buffer = vptr;
-    nleft  = n;
-
-    while ( nleft > 0 ) {
-        if ( (nwritten = write(sockd, buffer, nleft)) <= 0 ) {
-            if ( errno == EINTR )
-                nwritten = 0;
-            else
-                return -1;
-        }
-        nleft  -= nwritten;
-        buffer += nwritten;
-    }
-
-    return n;
-}
 
 char* ReadFile(char *filename)
 {
